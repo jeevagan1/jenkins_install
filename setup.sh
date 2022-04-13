097230ef976f518ae13a920c9eae5333f0a64eeb
Jeevagan <64369514+jeevagan1@users.noreply.github.com>
2022-04-13 15:39:07 +0530

    ini

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
+apt update 
+apt install wget unzip default-jdk screen tar -y 
+wget https://mirrors.tuna.tsinghua.edu.cn/jenkins/war-stable/2.332.2/jenkins.war
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
+mv ngrok-stable-linux-amd64.tgz /root
+mv jenkins.war /root
+tar -zxvf /root/ngrok-stable-linux-amd64.tgz
 
